5f789f75f8b344ba1509bab5a46337a73dc55dbf
Ron Rademaker <ron@connectholland.nl>
2015-11-20 12:11:07 +0100

    Added test case for issue with a $where in an aggregation $match

--- tests/AggregatedMongoPeerTest.php
+++ tests/AggregatedMongoPeerTest.php
@@ -2,6 +2,8 @@
 namespace ConnectHolland\Windmill\Addon\MongoAggregations\Test;
 
 use ConnectHolland\Windmill\Addon\MongoAggregations\MongoAggregation;
+use MongoCode;
+use MongoCriteria;
 use MongoPeerTest;
 use WMCommonRegistry;
 /**
@@ -19,4 +21,25 @@ class AggregatedMongoPeerTest extends MongoPeerTest
         WMCommonRegistry::get('serviceregistry')->set('mongo.aggregations', new MongoAggregation());
         parent::setUp();
     }
+
+    /**
+     * Add test case for a criteria with $where
+     *
+     * @return array
+     */
+    public function provideFilterCountData()
+    {
+        $testCases = parent::provideFilterCountData();
+
+        $mongoCriteria = new MongoCriteria();
+        $query = new MongoCode("function () { return true; }");
+        $mongoCriteria->add('$or', [['$where' => $query]], MongoCriteria::CUSTOM);
+
+        $new = $testCases[0];
+        $new[0] = $mongoCriteria;
+        $testCases[] = $new;
+
+        return $testCases;
+    }
+
 }
